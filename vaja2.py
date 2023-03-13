f3cce3ebb6dcac8e72c3671101a62329e65b5215
makovka44 <maj.mohar4@gmail.com>
2023-03-13 14:59:29 +0100

    dodane operacije

--- vaja2.py
+++ vaja2.py
@@ -6,28 +6,57 @@
 
 
 import random
-
-operacija=input("Vpiši 1 za seštevanje, 2 za odštevanje, 3 za množenj in 4 za deljenje.")
+pravilne = 0
+napacne = 0
+operacija=input("Vpiši 1 za seštevanje, 2 za odštevanje in 3 za množenje.")
 steviloracunov=input("Koliko računov želiš?")
 tocke = 0
 procenti = 0
+operacija = int(operacija)
 while steviloracunov != 0:
     if operacija == 1:
         stevilka1=random.randint(1, 10)
         stevilka2=random.randint(1, 10)
-        racun = input("Koliko je ", stevilka1, " x ", stevilka2, "?")
-
+        racun = input("Koliko je ", str(stevilka1), " + ", str(stevilka2), "?")
+        racun = int(racun)
         steviloracunov= steviloracunov -1
 
-        if racun== (stevilka1*stevilka2):
+        if racun== (stevilka1 + stevilka2):
             print("Odgovor je pravilen.")
             tocke = tocke + 1
+            pravilne =pravilne + 1
         else :
             print("Odgovor je nepravilen")
+            napacne = napacne + 1
+    if operacija == 2:
+        stevilka1=random.randint(1, 10)
+        stevilka2=random.randint(1, 10)
+        racun = input("Koliko je ", str(stevilka1), " - ", str(stevilka2), "?")
+        racun = int(racun)
+        steviloracunov= steviloracunov -1
 
+        if racun== (stevilka1 - stevilka2):
+            print("Odgovor je pravilen.")
+            tocke = tocke + 1
+            pravilne = pravilne + 1
+        else :
+            print("Odgovor je nepravilen")
+            napacne = napacne + 1
+    if operacija == 3:
+        stevilka1=random.randint(1, 10)
+        stevilka2=random.randint(1, 10)
+        racun = input("Koliko je ", str(stevilka1), " x ", str(stevilka2), "?")
+        racun = int(racun)
+        steviloracunov= steviloracunov -1
 
+        if racun == (stevilka1 * stevilka2):
+            print("Odgovor je pravilen.")
+            tocke = tocke + 1
+            pravilne = pravilne + 1
+        else :
+            print("Odgovor je nepravilen")
+            napacne = napacne + 1
 
-
-
-stevilkica=random.randint(1, 9)
-print(stevilkica)
+print("Dosegel si ", tocke, " stevilo točk.")
+procenti = (pravilne / steviloracunov) * 100
+print("Dosegel si ", procenti, " %.")
