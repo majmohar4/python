d16c0f150d11bbd08aa1c277e7bfcfa0f86830ed
makovka44 <maj.mohar4@gmail.com>
2023-03-14 08:45:18 +0100

    dodano guessing number game

--- number_guessing.py
+++ number_guessing.py
@@ -12,10 +12,11 @@ if poskusi != 0:
         print("Številka ni prava.")
         izbiranamiga = random.randint(1, 5)
         stevilo1 = random.randint(1, 5)
+        #veckratnik
         if izbiranamiga == 1:
             večkratnik = stevilka * stevilo1
             print("Večkratnik tega števila je" + večkratnik)
+        
         elif izbiranamiga == 2:
             while a >= 1:
-                stevilo2 = stevilo2 + 1
-                količnik = stevilka // stevilo2
+                
